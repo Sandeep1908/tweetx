ecb3e601005125b4da7cebe3d8be16e42f0c660d
Sandeep <sandbro7163337@gmail.com>
2024-01-20 14:12:52 +0530

    feat/app/add all route in app.js

--- tweetx/src/App.jsx
+++ tweetx/src/App.jsx
@@ -1,9 +1,31 @@
- import Main from "./pages/Main"
+ import Feed from "./pages/Feed"
+import Login from "./pages/Login"
+import Main from "./pages/Main"
+ import { createBrowserRouter,createRoutesFromElements ,RouterProvider,Route} from "react-router-dom"
+import Signup from "./pages/Signup"
+import Profile from "./pages/Profile"
+import User from "./pages/User"
 function App() {
  
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      
+      <Route path='/' element={<Main/>}>
+        <Route path="/" element={<Feed/>}/>
+        <Route path='/login'  element={<Login/>}/>
+        <Route path='/signup'  element={<Signup/>}/>
+        <Route path='/feed'  element={<Feed/>}/>
+        <Route path='/profile'  element={<Profile/>}/>
+        <Route path='/users'  element={<User/>}/>
+       </Route>
+
+
+
+    )
+  );
   return (
     <>
-        <Main/>
+       <RouterProvider router={router}/>
     </>
   )
 }
