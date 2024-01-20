d65572a638d37c01bbd3a16ac1c4d40420804386
Sandeep <sandbro7163337@gmail.com>
2024-01-20 14:17:57 +0530

    feat/pages/add Main component

--- tweetx/src/pages/Main.jsx
+++ tweetx/src/pages/Main.jsx
@@ -1,15 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Signup from './Signup'
 import Login from './Login'
 import Header from '../components/Header'
 import Feed from './Feed'
+import Profile from './Profile'
+import { Outlet, useLocation, useNavigate } from 'react-router-dom'
+import supabase from '../supabase/supabase'
 function Main() {
+  const [session,setSession]=useState(null)
+  const pathname=useLocation().pathname
+  const navigate=useNavigate()
+  const isHidden= pathname==='/login' || pathname==='/signup'
+
+  useEffect(()=>{
+    supabase.auth.onAuthStateChange((auth,_session)=>{
+      if(!isHidden && !_session){
+        navigate('/signup')
+      }
+    })
+  },[])
+
+ 
+
+
+ 
+ 
+ 
+ 
   return (
     <div className='w-full h-full'>
       <Header/>
-      <Feed/>
-        {/* <Signup/> */}
-        {/* <Login/> */}
+      <main>
+        <Outlet/>
+      </main>
     </div>
   )
 }
