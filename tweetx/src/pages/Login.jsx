0e28c38f7942dcbc0bfb8d193acaa15195eb8c50
Sandeep <sandbro7163337@gmail.com>
2024-01-20 14:17:42 +0530

    feat/pages/add Login page

--- tweetx/src/pages/Login.jsx
+++ tweetx/src/pages/Login.jsx
@@ -1,6 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import supabase from '../supabase/supabase'
 
 function Login() {
+    const navigate=useNavigate()
+    const [email,setEmail]=useState('')
+    const [password,setPassword]=useState('')
+
+    const handleLogin=()=>{
+        supabase.auth.signInWithPassword({
+            email,
+            password
+        })
+        .then((data)=>{
+            if(data?.error)return alert(data?.error)
+
+        
+            alert('login success')
+        
+            navigate('/feed')
+        })
+    }
+
   return (
     <div className="w-full h-full">
     <div className="max-w-7xl h-full m-auto flex justify-center items-center">
@@ -10,7 +31,7 @@ function Login() {
 
         <div className="flex flex-col justify-center items-start space-y-5">
           <h1 className="text-4xl text-[#FF758E] font-bold">TweetX</h1>
-          <button  className={`border-slate-400 border pr-16 pl-16 p-3 rounded-lg`}>Create Account</button>
+          <button  className={`border-slate-400 border pr-16 pl-16 p-3 rounded-lg` } onClick={()=>navigate('/signup')}>Create Account</button>
         </div>
 
           <div className="w-full h-full flex flex-col space-y-24">
@@ -22,17 +43,17 @@ function Login() {
                   
 
                     <div className="w-full">
-                      <input type="text" placeholder="Email" className="bg-[#F9F9F9] p-5 rounded-md w-full outline-none "/>
+                      <input type="text" placeholder="Email" className="bg-[#F9F9F9] p-5 rounded-md w-full outline-none " onChange={(e)=>setEmail(e.target.value)}/>
                     </div>
 
                     <div className="w-full">
-                      <input type="text" placeholder="Password" className="bg-[#F9F9F9] p-5 rounded-md w-full outline-none "/>
+                      <input type="password" placeholder="Password" className="bg-[#F9F9F9] p-5 rounded-md w-full outline-none " onChange={(e)=>setPassword(e.target.value)}/>
                     </div>
 
                     
 
                     <div className="w-full flex justify-end">
-                    <button   className={` pr-10 pl-10 p-3 rounded-lg text-white bg-[#FF758E]`}>Login</button>
+                    <button  onClick={handleLogin}  className={` pr-10 pl-10 p-3 rounded-lg text-white bg-[#FF758E]`}>Login</button>
                     </div>
                 </div>
           </div>
