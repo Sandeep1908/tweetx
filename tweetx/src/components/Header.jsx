a1b40451863d4a18c25411191721ef03f89006f8
Sandeep <sandbro7163337@gmail.com>
2024-01-20 14:17:25 +0530

    feat/componet/add header component

--- tweetx/src/components/Header.jsx
+++ tweetx/src/components/Header.jsx
@@ -1,16 +1,31 @@
-import React, { useState } from 'react'
-
+import React, { useEffect, useState } from 'react'
+import { Link, useLocation } from 'react-router-dom'
+import supabase from '../supabase/supabase'
 function Header() {
 
     const header=['Feed','Users','Profile']
     const [currentIndex,setCurrentIndex]=useState(0)
-
+    const [session,setSession]=useState(null)
+    
+    
     const handleClickHeader=(idx)=>{
         setCurrentIndex(idx)
     }   
+    const pathname=useLocation().pathname
+    useEffect(()=>{
+        supabase.auth.onAuthStateChange((_,_session)=>{
 
+          setSession(_session?.access_token)
+        })
+      
+
+
+      },[])
+
+      const isHidden= pathname==='/login' || pathname==='/signup'
+    
   return (
-    <div className='w-full h-20 bg-white shadow-lg   '>
+    <div className={`w-full h-20 bg-white shadow-lg  ${isHidden?'hidden':'block' } `}>
         <div className='max-w-7xl h-full  m-auto flex justify-between items-center'>
 
    
@@ -22,9 +37,10 @@ function Header() {
             <ul className='w-full flex justify-evenly'>
             {header?.map((item,idx)=>{
                 return(
-                    <li onClick={()=>handleClickHeader(idx)} className={`text-xl ${currentIndex===idx?'text-[#ff758e]':'text-slate-500 '} cursor-pointer` }>{item}</li>
+                    <Link to={`/${item}`} onClick={()=>handleClickHeader(idx)} className={`text-xl ${currentIndex===idx?'text-[#ff758e]':'text-slate-500 '} cursor-pointer` }>{item}</Link>
                 )
             })}
+            {session? <button   className={` pr-5 pl-5 p-2 rounded-lg text-white bg-[#FF758E]`} onClick={()=>supabase.auth.signOut()}>Log out</button>:''}
             </ul>
         </div>
         </div>
